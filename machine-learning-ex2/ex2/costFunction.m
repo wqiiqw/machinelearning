f4fd9cb379978be8367cd3418dd418d5914c1831
Wang Qi <13816989196@qq.com>
2017-01-14 18:05:15 +0800

    1) update the sigmoid.m 2) update the costFunction.m

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,6 +20,26 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% Wang Qi updated @ 2017/01/14
+
+
+tmp_sum = 0.0;
+for i=1:m
+  h_theta = sigmoid(X(i, :) * theta);
+  tmp_sum += -y(i)*log(h_theta) - (1 - y(i))*log(1-h_theta);
+endfor
+J = tmp_sum/m;
+
+
+for j=1:size(theta)
+  tmp_sum = 0.0;
+  for i=1:m
+     h_theta = sigmoid(X(i, :) * theta);
+     tmp_sum += (h_theta - y(i)) *  X(i, j);
+  grad(j) = tmp_sum / m;
+endfor
+
+% Wang Qi Update Ended
 
 
 
